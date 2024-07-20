abefc53826c2a67181f76009205519d07a5a3fda
Oleksandrov1995 <olexandrov1995@gmail.com>
2024-07-20 22:42:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/funbattery">
+    <BrowserRouter basename="oleksandrov1995.github.io/funbattery/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
